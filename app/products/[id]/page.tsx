14e4b7fd59ca21baa957ca2a39d690f6cce89c21
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-10-12 22:08:40 -0300

    update

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,8 +1,13 @@
 "use client"
 
-import React, { useState } from 'react'
-import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 import axios from 'axios'
+import { GoArrowLeft, GoUpload } from 'react-icons/go'
+import { CgClose } from 'react-icons/cg'
+import { FiCheck } from 'react-icons/fi'
+import Link from 'next/link'
+import Image from 'next/image'
 
 interface Product {
     _id: string,
@@ -16,26 +21,65 @@ interface Product {
     }[]
 }
 
+interface Category {
+    _id: string,
+    title: string
+}
+
 const Product = () => {
-    const router = useRouter()
-    const { id } = router.query
+    const id = usePathname();
 
     const [product, setProduct] = useState<Product>();
 
-    const getProduct = async () => {
-        try {
-            const response = await axios.get(`http://localhost:3333/products/${id}`);
-            setProduct(response.data);
-        } catch (error) {
-            console.log(error);
-        }
+useEffect(() => {
+    getProduct();
+}, [])
+
+const getProduct = async () => {
+    try {
+        const response = await axios.get(`http://localhost:3333${id}`);
+        setProduct(response.data);
+    } catch (error) {
+        console.log(error);
     }
+}
 
-    return (
-        <div>
+const getCategories = async () => {
+    try {
+      const response = await axios.get('http://localhost:3333/categories');
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+}; 
 
+return (
+    <div className='flex flex-col px-32 justify-center mb-16 w-full text-lg'>
+        <div className='mb-4 flex items-center'>
+            <Link href={"/products"}>
+                <GoArrowLeft className='text-2xl' />
+            </Link>
+            <span className='text-2xl ml-8'>{product?.title}</span>
         </div>
-    )
+        <div className='flex flex-col gap-2'>
+            <div className='flex flex-col gap-2'>
+                <label>Product Name</label>
+                <div>
+                    <span className='bg-white px-4 py-2 rounded-lg text-dark-gray'>{product?.title}</span>
+                </div>
+            </div>
+
+            <div className='flex flex-col gap-2'>
+                <label>Category</label>
+                <div>
+                {product?.categories.map((category, index) =>
+                        <span className='bg-white px-4 py-2 rounded-lg text-dark-gray' key={index}>{category}</span>
+                    )}
+                </div>
+            </div>
+        </div>
+    </div>
+)
 }
 
 export default Product
