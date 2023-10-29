181e2dc13e9864824f4a1b956bf6d2145b33af50
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-10-28 22:54:28 -0300

    update

--- app/products/[id]/edit/page.tsx
+++ app/products/[id]/edit/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'next/navigation'
 import axios from 'axios'
+import Image from 'next/image'
 
 interface Stock {
     color: string,
@@ -33,8 +34,8 @@ const Edit = () => {
     const [productName, setProductName] = useState<string>('');
     const [categories, setCategories] = useState<any[]>([]);
     // const [selectedCategory, setSelectedCategory] = useState<string>('');
-    const [selectedColor, setSelectedColor] = useState<string | null>(null);
-    const [selectedSize, setSelectedSize] = useState<number | null>(null);
+    const [selectedColor, setSelectedColor] = useState<string>("");
+    const [selectedSize, setSelectedSize] = useState<number>(0);
 
     useEffect(() => {
         getProduct();
@@ -80,7 +81,7 @@ const Edit = () => {
                     <label>Product Name</label>
                     <input className='bg-white p-2 rounded-lg placeholder-dark-gray' placeholder='Product Name' type="text" defaultValue={product?.title} onChange={(e) => setProductName(e.target.value)} required />
                 </div>
-                {/* <div className='flex flex-col'>
+                <div className='flex flex-col'>
                     <label>Category</label>
                     <select className='bg-white p-2 rounded-lg text-dark-gray' value={"Add New Color"}>
                         <option value="">Add New Category</option>
@@ -88,7 +89,42 @@ const Edit = () => {
                         <option key={index} value={category._id}>{category.title}</option>
                         )}
                     </select>
-                </div> */}
+                </div>
+                <div className='flex flex-col'>
+                    <label>Color</label>
+                    <select className='bg-white p-2 rounded-lg text-dark-gray' value={selectedColor}>
+                        <option value="">Add New Color</option>
+                        {product?.colors.map((color, index) => 
+                        <option key={index} value={color}>{color}</option>
+                        )}
+                    </select>
+                </div>
+                <div className='flex flex-col'>
+                    <label>Photos</label>
+                    <div className='flex flex-row gap-2'>
+                        {product?.images.map((image, index) => 
+                        <div key={index} className='flex flex-col'>
+                            <Image
+                                src={image.src}
+                                alt={""}
+                                width={160}
+                                height={160}
+                            />
+                        </div>
+                        )}
+                    </div>
+                </div>
+                    
+                <div className='flex flex-col'>
+                    <label>Size</label>
+                    <select className='bg-white p-2 rounded-lg text-dark-gray' value={selectedSize}>
+                        <option value="">Add New Size</option>
+                        {product?.sizes.map((size, index) => 
+                        <option key={index} value={size}>{size}</option>
+                        )}
+                    </select>
+                </div>
+                
             </form>
         </div>
     )
