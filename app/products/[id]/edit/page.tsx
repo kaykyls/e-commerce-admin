b610c0eac148a36aa0979cc6911c1b725864b48c
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-11-09 23:35:42 -0300

    update

--- app/products/[id]/edit/page.tsx
+++ app/products/[id]/edit/page.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link'
 import { GoArrowLeft } from 'react-icons/go'
 import { CgClose } from 'react-icons/cg'
 import { GoUpload } from 'react-icons/go'
+import { FiCheck } from 'react-icons/fi'
 
 interface Stock {
     color: string,
@@ -51,10 +52,16 @@ const Edit = () => {
     const [selectedColor, setSelectedColor] = useState<string>("");
     const [selectedSize, setSelectedSize] = useState<number>(0);
     const [selectedPhotos, setSelectedPhotos] = useState<{file: File, color: string}[]>([]);
+    const [isUploading, setIsUploading] = useState<boolean>(false);
+    const [productName, setProductName] = useState<string>("");
 
     useEffect(() => {
         getProduct();
         getCategories();
+
+        if (product) {
+            setProductName(product.title);
+        }
     }, [])
 
     const getProduct = async () => {
@@ -114,6 +121,10 @@ const Edit = () => {
     };
 
     const handleAddCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        if (product?.categories.includes(event.target.value)) {
+            return;
+        }
+
         const selectedCategory = event.target.value;
         if (selectedCategory) {
             const category = categories.find(category => category._id === selectedCategory);
@@ -145,17 +156,38 @@ const Edit = () => {
         }
     }
 
-    const handleSubmit = async (e: any) => {
-        // e.preventDefault();
-        // try {
-        //     const response = await axios.post('http://localhost:3333/products', {
-        //         title: productName,
-        //         categories: [selectedCategory]
-        //     });
-        //     console.log(response);
-        // } catch (error) {
-        //     console.log(error);
-        // }
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        setIsUploading(true);
+
+        const formData = new FormData();
+        formData.append('title', product.title);
+        formData.append('description', product.description);
+        formData.append('currentPrice', String(product.currentPrice));
+        formData.append('colors', JSON.stringify(product.colors));
+        formData.append('categories', JSON.stringify(product.categories));
+        formData.append('sizes', JSON.stringify(product.sizes));
+        formData.append('stock', JSON.stringify(product.stock));
+        formData.append('images', JSON.stringify(product.images));
+
+        selectedPhotos.forEach((photo) => {
+            formData.append('photos', photo.file);
+        });
+
+        try {
+            const response = await axios.put(`http://localhost:3333/products/${id}`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+
+        setIsUploading(false);
     }
     
     return (
@@ -270,20 +302,28 @@ const Edit = () => {
                 </div>
 
                 <div className='flex flex-col'>
-                    <label>Price</label>
-                    <input className='bg-white p-2 rounded-lg placeholder-dark-gray' placeholder='Price' type="number" defaultValue={product?.currentPrice} required />
-                </div>
-
-                <div className='flex flex-col'>
                     <label>Description</label>
                     <textarea className='bg-white p-2 rounded-lg placeholder-dark-gray' placeholder='Description' defaultValue={product?.description} required />
                 </div>
 
-                
-
-
-                
+                <div className='flex flex-col'>
+                    <label>Price</label>
+                    <input className='bg-white p-2 rounded-lg placeholder-dark-gray' placeholder='Price' type="number" defaultValue={product?.currentPrice} required />
+                </div>
             </form>
+
+            <div className='flex justify-end items-center gap-2 mt-4'>
+                <span>Finish</span>
+                <button className='bg-blue-700 text-neutral-50 rounded-full p-2' type="submit">{isUploading ?
+                    <div role="status">
+                        <svg aria-hidden="true" className="w-8 h-8 text-blue-600 animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/3000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                    </div> : <FiCheck className='text-3xl'/>
+                }</button>
+            </div>
         </div>
     )
 }
